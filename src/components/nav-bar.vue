a073391f0f518228e6f2bfd0f95e469fae37faaa
Ray Olivier <ray.olivier@projekt202.com>
2020-09-01 01:29:52 -0500

    basic nav bar component

---
+++ src/components/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background-color: darkslateblue;
+}
+</style>
