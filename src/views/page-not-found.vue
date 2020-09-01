908d2e85a2cc945104f1520064cc467402a0edf0
Ray Olivier <ray.olivier@projekt202.com>
2020-09-01 01:29:42 -0500

    add 404

---
+++ src/views/page-not-found.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>There's nothing here :(</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageNotFound"
+};
+</script>
+
+<style lang="less" scoped></style>
