b55efd71af5fec03100bb562a88967060a1f2a3b
Ray Olivier <ray.olivier@projekt202.com>
2020-08-31 22:07:24 -0500

    i need to decide how i want to do the home page

---
+++ src/components/home-page.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Random Weather App</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'HomePage',
+});
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HomePage />
+
+    <input v-model="newZipcode" @submit="addZipcode" placeholder="enter zipcode" />
+    <button type="button" @click="addZipcode">Add City</button>
+
+    <div class="city-card-container">
+      <CityCard v-for="zip in zipcodeList" :zipcode="zip" :key="zip" @delete="deleteCard" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HomePage from '@/components/home-page.vue'; // @ is an alias to /src
+import CityCard from '@/components/city-card.vue';
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    HomePage,
+    CityCard,
+  },
+  data() {
+    return {
+      newZipcode: '',
+      zipcodeList: [75007, 75035],
+    };
+  },
+  methods: {
+    addZipcode(event) {
+      event.preventDefault();
+      console.log('this.newZipcode', this.newZipcode);
+      if (this.newZipcode.length === 5) {
+        this.zipcodeList.push(parseInt(this.newZipcode));
+        this.newZipcode = '';
+      }
+    },
+    deleteCard(zip) {
+      this.zipcodeList.findIndex((x) => x === zip);
+    },
+  },
+};
 </script>
+
+<style scoped>
+.city-card-container {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+</style>
