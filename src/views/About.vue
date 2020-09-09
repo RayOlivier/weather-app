abb338be033ff9a3bedaf85820bcbd33f5929bbd
Ray Olivier <ray.olivier@projekt202.com>
2020-09-09 16:33:38 -0500

    update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>This is a small weather app, using the OpenWeather API.</p>
+    <h4>Dependencies:</h4>
+    <p>Vue, Vuetify, date-fns, Vuex, axios</p>
   </div>
 </template>
